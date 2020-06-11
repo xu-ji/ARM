a259f14d6184fc1f5e4bc055ac5e7def9771af4b
xuji <xuji@robots.ox.ac.uk>
2020-06-11 17:05:55 +0100

    .

--- code/util/load.py
+++ code/util/load.py
@@ -2,12 +2,14 @@ from .data import get_data
 from .general import get_device
 from code.models import *
 
+old_code_models = list(range(4821, 4821 + 5)) + list(range(2262, 2262 + 5))
+
 def get_model_and_data(config):
-  if config.data == "miniimagenet":
+  if config.model_ind in old_code_models: # reproduce random sampling of old code: remove in final version
     tasks_model = globals()[config.task_model_type](config).to(get_device(config.cuda))
     trainloader, testloader, valloader = get_data(config)
   else:
-    trainloader, testloader, valloader = get_data(config)  # mnist needs data size fields set first
+    trainloader, testloader, valloader = get_data(config)
     tasks_model = globals()[config.task_model_type](config).to(get_device(config.cuda))
 
   return tasks_model, trainloader, testloader, valloader
