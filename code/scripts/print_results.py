bf6b30d13a7769bfbddc689ef73bd38edf4b95c7
xuji <xuji@robots.ox.ac.uk>
2020-06-11 18:39:53 +0100

    .

--- code/scripts/print_results.py
+++ code/scripts/print_results.py
@@ -1,3 +1,79 @@
+import argparse
+import os
+import pickle
+import torch
+import numpy as np
+
+args = argparse.ArgumentParser(allow_abbrev=False)
+args.add_argument("--root", type=str, required=True)
+args.add_argument("--start", type=int, required=True)
+args.add_argument("--load_model", default=False, action="store_true")
+args.add_argument("--num_runs", type=int, default=5)
+args = args.parse_args()
+
+def treat_underscores(x):
+  res = []
+  for c in x:
+    if c == "_":
+      res.append("\\_")
+    else:
+      res.append(c)
+
+  return "".join(res)
 
 
-# print average test/val accuracy and forgetting only
+def print_results(args):
+  ms_avg = {"val": {"acc": [], "forgetting": [], "acc_binary": [], "forgetting_binary": []},
+            "test": {"acc": [], "forgetting": [], "acc_binary": [], "forgetting_binary": []}}
+
+  for m in range(args.start, args.start + args.num_runs):
+    print("---")
+    out_dir = os.path.join(args.root, str(m))
+    config_p = os.path.join(out_dir, "config.pickle")
+
+    config = None
+    tries = 0
+    while tries < 1000:
+      try:
+        with open(config_p, "rb") as config_f:
+          config = pickle.load(config_f)
+        break
+      except:
+        tries += 1
+
+    if config is None:
+      continue
+
+    if args.load_model:
+      torch.load(os.path.join(config.out_dir, "latest_models.pytorch"))
+
+    actual_t = config.max_t
+    for prefix in ["val", "test"]:
+      accs_dict = getattr(config, "%s_accs" % prefix)
+
+      ms_avg[prefix]["acc"].append(accs_dict[actual_t])
+
+      forgetting_dict = getattr(config, "%s_forgetting" % prefix)
+      if actual_t in forgetting_dict:
+        ms_avg[prefix]["forgetting"].append(forgetting_dict[actual_t])
+
+      print("model %d, %s: acc %.4f, forgetting %.4f" % (
+      config.model_ind, prefix, accs_dict[actual_t], forgetting_dict[actual_t]))
+
+    print("---")
+
+  for prefix in ["val", "test"]:
+    for metric in ["acc", "forgetting"]:
+      if len(ms_avg[prefix][metric]) == 0:
+        ms_avg[prefix][metric] = (-1, -1)
+      else:
+        avg = np.array(ms_avg[prefix][metric]).mean()
+        std = np.array(ms_avg[prefix][metric]).std()
+        ms_avg[prefix][metric] = (avg, std)
+
+    print("average %s: acc %.4f, forgetting %.4f" % (
+    prefix, ms_avg[prefix]["acc"], ms_avg[prefix]["forgetting"]))
+
+
+if __name__ == "__main__":
+  print_results(args)
