fe2c39939f5133f37227fe2e114af140443d830c
xuji <xuji@robots.ox.ac.uk>
2020-06-16 20:59:56 +0100

    .

--- code/util/load.py
+++ code/util/load.py
@@ -3,7 +3,7 @@ from .general import get_device
 from code.models import *
 
 def get_model_and_data(config):
-  trainloader, testloader, valloader = get_data(config)
   tasks_model = globals()[config.task_model_type](config).to(get_device(config.cuda))
+  trainloader, testloader, valloader = get_data(config)
 
   return tasks_model, trainloader, testloader, valloader
