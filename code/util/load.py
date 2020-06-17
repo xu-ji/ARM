2f1b079cf1dd0f2e662e0dbdc64215b303486790
xuji <xuji@robots.ox.ac.uk>
2020-06-18 00:03:07 +0100

    .

--- code/util/load.py
+++ code/util/load.py
@@ -4,6 +4,7 @@ from code.models import *
 
 def get_model_and_data(config):
   config.task_in_dims = {"mnist5k": (28 * 28,), "miniimagenet": (3, 84, 84), "cifar10": (3, 32, 32)}[config.data]
+  config.task_out_dims = {"mnist5k": (10,), "miniimagenet": (100,), "cifar10": (10,)}[config.data]
 
   tasks_model = globals()[config.task_model_type](config).to(get_device(config.cuda))
 
