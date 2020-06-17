580a197687679b81539196da107b1c2d7f73c3a9
xuji <xuji@robots.ox.ac.uk>
2020-06-17 23:52:59 +0100

    .

--- code/util/load.py
+++ code/util/load.py
@@ -3,8 +3,12 @@ from .general import get_device, invert_dict
 from code.models import *
 
 def get_model_and_data(config):
+  config.task_in_dims = {"mnist5k": (28 * 28,), "miniimagenet": (3, 84, 84), "cifar10": (3, 32, 32)}[config.data]
+
   tasks_model = globals()[config.task_model_type](config).to(get_device(config.cuda))
+
   trainloader, testloader, valloader = get_data(config)
+
   config.class_dict_tasks = invert_dict(trainloader.dataset.task_dict_classes)
 
   return tasks_model, trainloader, testloader, valloader
