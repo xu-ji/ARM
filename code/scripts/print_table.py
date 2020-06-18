2ff81aeb0fc3882d33e47b0eb453f833b7cd9645
xuji <xuji@robots.ox.ac.uk>
2020-06-18 15:59:44 +0100

    .

---
+++ code/scripts/print_table.py
@@ -0,0 +1,94 @@
+import torch
+import os
+import argparse
+import pickle
+import numpy as np
+
+args = argparse.ArgumentParser(allow_abbrev=False)
+args.add_argument("--root", type=str, required=True)
+args.add_argument("--num_runs", type=int, default=5)
+args = args.parse_args()
+
+experiments = [
+  ("ARM MNIST", 6579),
+  ("ARM Cifar10", 3717),
+  ("ARM MiniImageNet", 4821),
+
+  ("ADI MNIST", 6232),
+  ("ADI Cifar10", 2262),
+  ("ADI MiniImageNet", 6042),
+
+  ("Distill MNIST", 6102),
+  ("Distill Cifar10", 4082),
+  ("Distill MiniImageNet", 6092),
+
+  ("Naive MNIST", 4967),
+  ("Naive Cifar10", 2522),
+  ("Naive MiniImageNet", 4557),
+
+  # table 4
+  ("Distill Cifar10, unit lag", 6452),
+  ("ADI Cifar10, unit lag", 6467),
+  ("ADI Cifar10, no distill", 2327),
+  ("ARM Cifar10, unit lag", 4127),
+  ("ARM Cifar10, no distill", 2717),
+
+  # table 7
+]
+
+num_runs = 5
+
+ms_avg = {"val": {"acc": [], "forgetting": [], "acc_binary": [], "forgetting_binary": []},
+            "test": {"acc": [], "forgetting": [], "acc_binary": [], "forgetting_binary": []}}
+
+print("& \multicolumn{2}{c}{Val} & \multicolumn{2}{c}{Test} \\\\")
+print("& acc & forgetting & acc & forgetting \\\\")
+print("\midrule")
+for name, m_start in experiments:
+
+  for m in range(m_start, m_start + args.num_runs):
+    out_dir = os.path.join(args.root, str(m))
+    config_p = os.path.join(out_dir, "config.pickle")
+
+    config = None
+    tries = 0
+    while tries < 1000:
+      try:
+        with open(config_p, "rb") as config_f:
+          config = pickle.load(config_f)
+        break
+      except:
+        tries += 1
+
+    if config is None:
+      continue
+
+    actual_t = config.max_t
+    for prefix in ["val", "test"]:
+      accs_dict = getattr(config, "%s_accs" % prefix)
+
+      ms_avg[prefix]["acc"].append(accs_dict[actual_t])
+
+      forgetting_dict = getattr(config, "%s_forgetting" % prefix)
+      if actual_t in forgetting_dict:
+        ms_avg[prefix]["forgetting"].append(forgetting_dict[actual_t])
+
+  for prefix in ["val", "test"]:
+    for metric in ["acc", "forgetting"]:
+      if len(ms_avg[prefix][metric]) == 0:
+        ms_avg[prefix][metric] = (-1, -1)
+      else:
+        avg = np.array(ms_avg[prefix][metric]).mean()
+        std = np.array(ms_avg[prefix][metric]).std()
+        ms_avg[prefix][metric] = (avg, std)
+
+  print("%s & %.4f $\pm$ %.4f & %.4f $\pm$ %.4f & %.4f $\pm$ %.4f & %.4f $\pm$ %.4f \\\\" %
+        (name,
+
+         ms_avg["val"]["acc"][0], ms_avg["val"]["acc"][1],
+         ms_avg["val"]["forgetting"][0], ms_avg["val"]["forgetting"][1],
+
+         ms_avg["test"]["acc"][0], ms_avg["test"]["acc"][1],
+         ms_avg["test"]["forgetting"][0], ms_avg["test"]["forgetting"][1],
+         ))
+
